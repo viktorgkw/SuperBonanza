92543223e15d4840ea9c3d53b95db6faa9ab23b2
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2024-03-24 11:29:04 +0200

    Moved migrations folder and added IntegratorEvent

---
+++ src/Common/Common.RabbitMQ/Events/IntegratorEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.RabbitMQ.Events;
+
+public record IntegratorEvent
+{
+    public Guid Id => Guid.NewGuid();
+
+    public DateTime OccuredOn => DateTime.UtcNow;
+
+    public string EventType => GetType().AssemblyQualifiedName;
+}
